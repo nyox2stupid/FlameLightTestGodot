d512416d3e2485901854ac6f68e761676e80aa08
nyox2stupid <patriwachs@gmail.com>
2024-04-22 13:41:24 +0200

    Hid mouse

--- Scripts/Flame.gd
+++ Scripts/Flame.gd
@@ -5,6 +5,8 @@ var movement_dir = 'm'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
 	var mouse_pos_old = get_viewport().get_mouse_position()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
