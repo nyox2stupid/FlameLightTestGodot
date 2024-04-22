ec58a88471188706bd8ef859062a41e5a0440ff3
nyox2stupid <patriwachs@gmail.com>
2024-04-22 12:13:39 +0200

    added movement direction detection

--- Scripts/Flame.gd
+++ Scripts/Flame.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var movement_dir = 'm'
-
+	var mouse_pos_old = get_viewport().get_mouse_position()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,5 +13,14 @@ func _process(delta):
 	
 	# Set the position of the current node to the mouse position.
 	position = mouse_pos
-	
-	
+
+
+	if mouse_pos[0]>mouse_pos_old[0]:
+		movement_dir = 'r'
+	else if mouse_pos[0]<mouse_pos_old[0]:
+		movement_dir = 'l'
+	else:
+		movement_dir = 'm'
+
+	print(movement_dir)
+	var mouse_pos_old = get_viewport().get_mouse_position()
